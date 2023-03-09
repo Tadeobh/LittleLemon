bae0c0c8f8867af2c9ff77e43335376198f26982
Tadeo Bustamante <tadeohb@gmail.com>
2023-03-08 19:30:35 -0700

    Implemented ViewSet for Booking objects
    Added the ViewSet for Booking objects, and registered the endpoints in the project-level url.py file to be able to perform CRUD operations on them.

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Menu, Booking
-from .serializers import MenuSerializer
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 class MenuItemView(ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
+
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
